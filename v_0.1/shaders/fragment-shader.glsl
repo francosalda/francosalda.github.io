4b88d34a3ac95cc0feb3d122601cfa73abd9ec08
francosalda <43392793+francosalda@users.noreply.github.com>
2022-07-16 13:33:22 -0300

    Implemento 6 luces SPOT

--- v_0.1/shaders/fragment-shader.glsl
+++ v_0.1/shaders/fragment-shader.glsl
@@ -5,35 +5,59 @@
         varying vec3 vWorldPosition;
         varying vec3 vFixedColorObject;
     
-         
-    
-        
-  
 
-        //Iluminacion
-        uniform vec3 uAmbientColor;         // color de luz ambiente
-        uniform vec3 uLightPositionLoc; // posicion del spot de luz
-        uniform vec3 uLightDirectionLoc; // direccion del spot
-        uniform float uLightInnerCutOffLoc; // angulo interno 
-        uniform float uLightOutterCutOffLoc; // angulo externo
         uniform bool uUseLighting;          // usar iluminacion si/no
+        //Luces spots
+        uniform vec3 uAmbientColor;         // color de luz ambiente
+        uniform vec3 uLightPositionSpot1; // posicion del spot de luz1
+        uniform vec3 uLightPositionSpot2; // posicion del spot de luz2
+        uniform vec3 uLightPositionSpot3; // posicion del spot de luz3
+        uniform vec3 uLightPositionSpot4; // posicion del spot de luz4
+        uniform vec3 uLightPositionSpot5; // posicion del spot de luz5
+        uniform vec3 uLightPositionSpot6; // posicion del spot de luz6
+        uniform vec3 uLightDirectionSpot; // direccion del spot
+        uniform float uLightInnerCutOffSpot; // angulo interno 
+        uniform float uLightOutterCutOffSpot; // angulo externo
 
+        
         uniform sampler2D uSampler_0;       //sampler de textura
+        //calcula la luz de cada spot
+        float calculateBrightnessSpot(vec3 posicionLuz)
+        {
+            vec3 pointToLightOffset =  posicionLuz -vWorldPosition;
+            vec3 pointToLightDirection = normalize(pointToLightOffset);
+            vec3 lightToPointDirection = -pointToLightDirection;
+            //spotlight
+            float diffuse = max(0.0,dot(pointToLightDirection,vNormal));
+            float angleToSurface = dot(uLightDirectionSpot,lightToPointDirection);
+            float spot = smoothstep(uLightOutterCutOffSpot,uLightInnerCutOffSpot,angleToSurface);
+            float brightnessSpot = diffuse * spot;
+            return brightnessSpot;
+
+        }
+
+        //suma el aporte de todas las fuentes de luces 
+        float calculateBrightness()
+        {
+            float brightnessSpot1  = calculateBrightnessSpot(uLightPositionSpot1);
+            float brightnessSpot2  = calculateBrightnessSpot(uLightPositionSpot2);
+            float brightnessSpot3  = calculateBrightnessSpot(uLightPositionSpot3);
+            float brightnessSpot4  = calculateBrightnessSpot(uLightPositionSpot4);
+            float brightnessSpot5  = calculateBrightnessSpot(uLightPositionSpot5);
+            float brightnessSpot6  = calculateBrightnessSpot(uLightPositionSpot6);
+
+            return brightnessSpot1+brightnessSpot2+brightnessSpot3+brightnessSpot4+brightnessSpot5+brightnessSpot6;
+            
+        }
+        
 
 
         void main(void) {        
 
             if (uUseLighting)
             {
-
-            vec3 pointToLightOffset =  uLightPositionLoc -vWorldPosition;
-            vec3 pointToLightDirection = normalize(pointToLightOffset);
-            vec3 lightToPointDirection = -pointToLightDirection;
-            //spotlight
-            float diffuse = max(0.0,dot(pointToLightDirection,vNormal));
-            float angleToSurface = dot(uLightDirectionLoc,lightToPointDirection);
-            float spot = smoothstep(uLightOutterCutOffLoc,uLightInnerCutOffLoc,angleToSurface);
-            float brightness = diffuse * spot;
+            
+            float brightness = calculateBrightness();
             gl_FragColor = (texture2D(uSampler_0, vUv)*(uAmbientColor,1.0))*.2+(texture2D(uSampler_0, vUv)* brightness) * 0.8; //20% luz ambiente+ 80% luz de fuentes 
             gl_FragColor.a = 1.0;
 
